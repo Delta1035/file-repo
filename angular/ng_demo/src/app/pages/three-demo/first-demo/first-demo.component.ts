2f31fb6bde63d08227067b1abc526238158f75f9
chongyang.zheng <528491526@qq.com>
2023-06-01 18:22:57 +0800

    css

--- angular/ng_demo/src/app/pages/three-demo/first-demo/first-demo.component.ts
+++ angular/ng_demo/src/app/pages/three-demo/first-demo/first-demo.component.ts
@@ -25,10 +25,9 @@ export class FirstDemoComponent implements AfterViewInit {
     const containerElement: HTMLDivElement = this.container.nativeElement;
     const width = containerElement.clientWidth;
     const height = containerElement.clientHeight;
-
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    camera.lookAt(vector)
+    // camera.lookAt(vector)
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(width, height);
     this.render.appendChild(containerElement, renderer.domElement);
@@ -36,18 +35,13 @@ export class FirstDemoComponent implements AfterViewInit {
     const material = new THREE.MeshBasicMaterial({ color: 0xfff });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
-
     camera.position.z = 5;
-
     function animate() {
       requestAnimationFrame(animate);
-
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
-
       renderer.render(scene, camera);
     }
-
     animate();
   }
 }
---
+++ learnCss/css.md
@@ -0,0 +1,24 @@
+## BEM
+
+#### block（块）
+
+### element（元素__）
+
+### modifier（修饰符--）
+
+
+
+## SMACSS 
+
+分类：Base，Layout，Module，State，Theme
+
+好处： 维护，复用，扩展
+
+命名规范：.l-header,.is-close,.theme-nav
+
+1. base 浏览器样式重置 normalize.css
+2. layout 页面常见布局
+3. module 页面某个区域的样式
+4. state 模块的状态
+5. theme 主题样式
+
