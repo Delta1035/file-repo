2b9ad0be6a687975a4f31ca6923c86240b38ff50
chongyang.zheng <chongyang.zheng@jmalltech.com>
2023-10-18 11:06:44 +0800

    feat✨: zone

--- angular/zone/index.ts
+++ angular/zone/index.ts
@@ -1,11 +1,18 @@
 // @ts-ignore
 import('zone.js');
-const z = Zone.current.fork({
-    name:'judgement',
-    properties:{
-        name:'z'
-    },
-    onHasTask:function(){
-        console.log('judgement感知到了异步任务调用');
-    }
-})
+let rootZone = Zone.current;
+let zoneA = rootZone.fork({
+    name: 'zoneA'
+});
+
+zoneA.run(function () {
+    newFunction();
+});
+
+
+
+function newFunction () {
+    setTimeout(() => {
+    });
+}
+
